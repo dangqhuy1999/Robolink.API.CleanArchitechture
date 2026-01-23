c991c508cf9883d4fae5eceaef4609eb84aaf7b4
huydang <dangqhuy1999@gmail.com>
2026-01-23 14:55:34 +0700

    Add project files.

---
+++ Robolink.API/Program.cs
@@ -0,0 +1,27 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer(); // Cần thiết để Swagger tìm thấy các Endpoint
+builder.Services.AddSwaggerGen();           // ĐĂNG KÝ provider cho Swagger
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(); // Kích hoạt giao diện đồ họa https://localhost:7241/swagger/index.html
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
